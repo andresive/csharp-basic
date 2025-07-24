2d69c9b75138cc16e52c3ab73f18efaa896cf8f9
andresive <gabrielgonzagga41@outlook.com>
2025-07-24 04:13:57 -0300

    add aula12 about while

---
+++ aula12/Program.cs
@@ -0,0 +1,20 @@
+﻿// aula sobre while
+
+
+namespace aula12;
+
+public class Program
+{
+    public static void Main()
+    {
+        double budget = 100;
+
+        // enquanto budget for maior que 0 {...
+        while (budget > 0) 
+        {
+            Console.WriteLine(budget);
+            budget--;
+        }
+        Console.WriteLine("Atenção saldo insuficiente!!");
+    }
+}
